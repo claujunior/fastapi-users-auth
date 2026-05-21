ec1c2fafb705203d6e0b1cec178a7d6ca50f56e1
claujunior <claudivanjunior2002@hotmail.com>
2026-05-21 13:35:08 -0300

    hotfix

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"
-        ],
-    allow_origin_regex=r"http://localhost:.*",  
+    allow_origins=["*"], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
