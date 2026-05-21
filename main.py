d558a862cb1aa966d9b3c472990a089156dc0b3b
claujunior <claudivanjunior2002@hotmail.com>
2026-05-21 09:58:48 -0300

    cors

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500",
-        "http://localhost:5500"],  
+    allow_origins=["http://127.0.0.1:5500"
+        ],
+    allow_origin_regex=r"http://localhost:.*",  
     allow_methods=["*"],
     allow_headers=["*"],
 )
